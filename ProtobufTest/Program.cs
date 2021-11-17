d5d6c813998ebc661654cdb07a0a87d7a61f0033
BunnyBlack <xyy6836@126.com>
2021-11-17 15:55:56 +0800

    update

--- ProtobufTest/Program.cs
+++ ProtobufTest/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ProtobufTest
+﻿using System;
+
+namespace ProtobufTest
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var message = new SimpleMessage();
+            message.Name = "name";
+            Console.WriteLine(string.IsNullOrEmpty(message.Name));
+            Console.WriteLine(message.TestOneofCase);
+            message.SubMessage = new SubMessage();
+            Console.WriteLine(string.IsNullOrEmpty(message.Name));
+            Console.WriteLine(message.TestOneofCase);
         }
     }
 }
